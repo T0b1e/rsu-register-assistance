00d1272b0b812aced89fd9bffe39eb3073708e2e
T0b1e <narongkorn.tob@gmail.com>
2024-11-12 21:34:19 +0700

    adding new classes

--- src/data.js
+++ src/data.js
@@ -5,82 +5,178 @@ export const data = {
             "name": "COMPUTER ORGANIZATION AND ARCHITECTURE",
             "code": "CPE432",
             "schedule": [
-                { "sec": "01", "day": "อังคาร", "time": "12:00 - 14:50" },
-                { "sec": "02", "day": "พฤหัส", "time": "12:00 - 14:50" }
+              {"sec": "1", "day": "อังคาร", "time": "12:00 - 14:50" },
+              {"sec": "2", "day": "พฤหัส", "time": "12:00 - 14:50" }
             ],
             "type": "ทฤษฎี",
-            "price": 7200,
             "credits": 3
-        },
-        {
+          },
+          {
             "name": "FUNDAMENTAL OF DATABASE SYSTEMS",
             "code": "CPE361",
             "schedule": [
-                { "sec": "01", "day": "อังคาร", "time": "15:00 - 17:50" },
-                { "sec": "02", "day": "ศุกร์", "time": "12:00 - 14:50" }
+              {"sec": "1", "day": "อังคาร", "time": "15:00 - 17:50" },
+              {"sec": "2", "day": "ศุกร์", "time": "12:00 - 14:50" }
             ],
             "type": "ทฤษฎี",
-            "price": 7200,
             "credits": 3
-        },
-        {
+          },
+          {
             "name": "SIGNALS AND SYSTEMS",
             "code": "CPE308",
             "schedule": [
-                { "sec": "01", "day": "พุธ", "time": "09:00 - 11:50" },
-                { "sec": "02", "day": "ศุกร์", "time": "12:00 - 14:50" }
+              {"sec": "1", "day": "พุธ", "time": "09:00 - 11:50" },
+              {"sec": "2", "day": "ศุกร์", "time": "12:00 - 14:50" }
             ],
             "type": "ทฤษฎี",
-            "price": 7200,
             "credits": 3
-        },
-        {
+          },
+          {
             "name": "COMPUTER ENGINEERING MATHEMATICS II",
             "code": "CPE332",
             "schedule": [
-                { "sec": "01", "day": "จันทร์", "time": "12:00 - 14:50" },
-                { "sec": "02", "day": "อังคาร", "time": "09:00 - 11:50" }
+              {"sec": "1", "day": "จันทร์", "time": "12:00 - 14:50" },
+              {"sec": "2", "day": "อังคาร", "time": "09:00 - 11:50" }
             ],
             "type": "ทฤษฎี",
-            "price": 7200,
             "credits": 3
-        },
-        {
+          },
+          {
             "name": "DATA COMMUNICATION AND DATA NETWORKS",
             "code": "CPE326",
             "schedule": [
-                { "sec": "01", "day": "พุธ", "time": "15:00 - 17:50" },
-                { "sec": "02", "day": "พฤหัส", "time": "15:00 - 17:50" }
+              {"sec": "1", "day": "พุธ", "time": "15:00 - 17:50" },
+              {"sec": "2", "day": "พฤหัส", "time": "15:00 - 17:50" }
             ],
             "type": "ทฤษฎี",
-            "price": 7200,
             "credits": 3
-        },
-        {
+          },
+          {
             "name": "ENGINEERING MANAGEMENT",
             "code": "IEN301",
+            "sec": "1",
             "schedule": [
-                { "sec": "01", "day": "จันทร์", "time": "15:00 - 17:50" },
-                { "sec": "02", "day": "พฤหัส", "time": "16:00 - 18:50" }
+              { "day": "อังคาร", "time": "09:00 - 11:50" },
+              { "day": "อังคาร", "time": "12:00 - 14:50" }
             ],
-            "type": "ทฤษฎี",
-            "price": 7200,
+            "type": "ปฎิบัติ",
             "credits": 3
-        },
-        {
+          },
+          {
             "name": "DATA COMMUNICATION LABORATORY",
             "code": "CPE327",
             "schedule": [
-                { "sec": "11", "day": "อังคาร", "time": "09:00 - 11:50" },
-                { "sec": "12", "day": "อังคาร", "time": "12:00 - 14:50" },
-                { "sec": "13", "day": "พุธ", "time": "09:00 - 11:50" },
-                { "sec": "14", "day": "พุธ", "time": "12:00 - 14:50" },
-                { "sec": "15", "day": "พฤหัส", "time": "09:00 - 11:50" }
+              {"sec": "11", "day": "พุธ", "time": "09:00 - 11:51" },
+              {"sec": "12", "day": "พุธ", "time": "12:00 - 14:51" },
+              {"sec": "13", "day": "พฤหัส", "time": "09:00 - 11:52" },
+              {"sec": "14", "day": "จันทร์", "time": "15:00 - 17:50" },
+              {"sec": "15", "day": "พฤหัส", "time": "16:00 - 18:50" }
+            ],
+            "type": "ทฤษฎี",
+            "credits": 1
+          },
+      
+      
+      
+      
+          {
+            "name": "Application Development",
+            "code": "CPE363",
+            "schedule": [
+              {"sec": "1", "day": "อังคาร", "time": "13:00 - 16:50" },
+              {"sec": "2", "day": "พุธ", "time": "13:00 - 16:50" },
+              {"sec": "3", "day": "จันทร์", "time": "13:00 - 16:50" }
+            ],
+            "type": "ทฤษฎี",
+            "credits": 3
+          },
+          {
+            "name": "Computer Operating Systems",
+            "code": "CPE419",
+            "schedule": [
+              {"sec": "1", "day": "พฤหัส", "time": "09:00 - 11:50" },
+              {"sec": "2", "day": "พุธ", "time": "12:00 - 14:50" }
+            ],
+            "type": "ทฤษฎี",
+            "credits": 3
+          },
+          {
+            "name": "Computer Networks",
+            "code": "CPE426",
+            "schedule": [
+              {"sec": "1", "day": "พุธ", "time": "09:00 - 11:50" },
+              {"sec": "2", "day": "ศุกร์", "time": "12:00 - 14:50" }
             ],
             "type": "ปฎิบัติ",
-            "price": 6400,
             "credits": 1
-        },
+          },
+          {
+            "name": "Computer Networks Laboratory",
+            "code": "CPE427",
+            "schedule": [
+              {"sec": "11", "day": "อังคาร", "time": "09:00 - 11:50" },
+              {"sec": "12", "day": "พฤหัส", "time": "12:00 - 14:50" },
+              {"sec": "13", "day": "พฤหัส", "time": "09:00 - 11:50" },
+              {"sec": "14", "day": "ศุกร์", "time": "12:00 - 14:50" }
+            ],
+            "type": "ปฎิบัติ",
+            "credits": 1
+          },
+          {
+            "name": "System and Software Engineering",
+            "code": "CPE441",
+            "schedule": [
+              {"sec": "1", "day": "จันทร์", "time": "12:00 - 14:50" },
+              {"sec": "2", "day": "อังคาร", "time": "12:00 - 14:50" }
+            ],
+            "type": "ทฤษฎี",
+            "credits": 3
+          },
+      
+      
+      
+      
+          {
+            "name": "COMPUTER ENGINEERING MATHEMATICS 1",
+            "code": "CPE231",
+            "schedule": [
+              {"sec": "1", "day": "จันทร์", "time": "13:00 - 15:50" }
+            ],
+            "type": "ทฤษฎี",
+            "credits": 3
+          },
+          {
+            "name": "ALGORITHMS AND DATA STRUCTURES",
+            "code": "CPE360",
+            "schedule": [
+              {"sec": "1", "day": "พฤหัส", "time": "13:00 - 15:50" },
+              {"sec": "2", "day": "ศุกร์", "time": "13:00 - 15:50" }
+            ],
+            "type": "ทฤษฎี",
+            "credits": 3
+          },
+          {
+            "name": "ELECTRONIC CIRCUIT ENGINEERING",
+            "code": "CPE225",
+            "schedule": [
+              {"sec": "1", "day": "จันทร์", "time": "09:00 - 11:50" },
+              {"sec": "2", "day": "อังคาร", "time": "09:00 - 11:50" }
+            ],
+            "type": "ทฤษฎี",
+            "credits": 3
+          },
+          {
+            "name": "ENGINEERING PHYSICS",
+            "code": "PHY116",
+            "schedule": [
+              {"sec": "1", "day": "จันทร์", "time": "09:00 - 11:50" },
+              {"sec": "2", "day": "อังคาร", "time": "09:00 - 11:50" },
+              {"sec": "3", "day": "พุธ", "time": "09:00 - 11:50" },
+              {"sec": "4", "day": "ศุกร์", "time": "09:00 - 11:50" }
+            ],
+            "type": "ทฤษฎี",
+            "credits": 3
+          }
     ]
 };
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 // src/main.js
 import { createCourseTable, createNewTable, updateSubjectCounter, loadPlansFromLocalStorage } from './functions.js';
 import { populateCourseDropdown, handleFilterChange } from './components/filter.js';
-import { startClock } from './functions.js';
+// import { startClock } from './functions.js';
 import './events.js';
 
 document.addEventListener('DOMContentLoaded', () => {
-    startClock();
+    // startClock();
     loadPlansFromLocalStorage();
     updateSubjectCounter();
     populateCourseDropdown();
